0d0f505231987eb8d613ab68b49de91088af2e5c
Christian Picar <christianpicar23@gmail.com>
2026-07-25 23:20:12 +0800

    skybox test

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
     public string gameSceneName = "GameScene";
-
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration = 1f;
     public void StartGame()
     {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    private IEnumerator FadeAndLoadScene()
+     {
+
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            fadeCanvasGroup.alpha = Mathf.Clamp01(t / fadeDuration);
+            yield return null;
+        }
+
+        fadeCanvasGroup.alpha = 1f;
         SceneManager.LoadScene(gameSceneName);
     }
 }
